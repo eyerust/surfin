bd6a15059c71a81d554e222b2f4e1e6de23ef523
Michael Danielyan <32106337+eyerust@users.noreply.github.com>
2024-12-10 11:18:21 +0900

    Return back to upstream

--- undo_bluefin_changes.sh
+++ undo_bluefin_changes.sh
@@ -4,7 +4,6 @@ set -ouex pipefail
 
 # Remove Repos
 rm /etc/yum.repos.d/_copr_lukenukem-asus-linux.repo
-rm /etc/yum.repos.d/linux-surface.repo
 
 # Remove packages
 ASUS_PACKAGES=(
@@ -13,7 +12,6 @@ ASUS_PACKAGES=(
 )
 
 SURFACE_PACKAGES=(
-    iptsd
     libcamera
     libcamera-tools
     libcamera-gstreamer
@@ -24,19 +22,5 @@ SURFACE_PACKAGES=(
 rpm --erase --nodeps \
     "${ASUS_PACKAGES[@]}" \
     "${SURFACE_PACKAGES[@]}"
-
-# Remove modules
-rm /usr/lib/modules-load.d/ublue-surface.conf
-
-# Remove Kernel
-for pkg in kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
-do
-    rpm --erase $pkg --nodeps
-done
-
-# Remove AKMOD packages
-rpm -qa | grep -E '^(xone|xpadneo|openrazer|framework-laptop)' | xargs rpm --erase --nodeps
-
-rpm -qa | grep -E '^(broadcom-wl|wl|v4l2loopback)' | xargs rpm --erase --nodeps
 
 echo "Removed all bluefin changes"
